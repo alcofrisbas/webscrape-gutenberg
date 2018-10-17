24a54075a12f760d1771bc835abb30ee21e6bd39
alcofrisbas <bgreene231@gmail.com>
2018-10-17 13:18:10 -0500

    trivial thing

--- scraper.py
+++ scraper.py
@@ -7,6 +7,7 @@ def scrape_num(n):
     url = BASE_URL.replace("[a]", str(n))
     url = url.replace("[b]", str(n))
     print(url)
+    return None
 
 if __name__ == "__main__":
     scrape_num(7138)
