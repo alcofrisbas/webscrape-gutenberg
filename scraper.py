af2c028d3fa5b722443c149d5281b4e5673a8b8b
alcofrisbas <bgreene231@gmail.com>
2018-10-18 14:05:38 -0500

    not sure why not commited

--- scraper.py
+++ scraper.py
@@ -1,14 +1,20 @@
-import urllib
+import urllib.request
+import sys
 # import nltk
 
-BASE_URL = "[a][b]"
+BASE_URL = "https://www.gutenberg.org/files/[a]/[b].txt"
 
 def scrape_num(n):
     url = BASE_URL.replace("[a]", str(n))
     url = url.replace("[b]", str(n))
     print(url)
+    with urllib.request.urlopen(url) as r:
+        text = r.read().decode("utf-8")
 
+    return(text)
 
 if __name__ == "__main__":
-    scrape_num(7138)
-
+    args = sys.argv[1:]
+    t = scrape_num(args[0])
+    print(len(t))
+    print(t.split("\n")[0])
